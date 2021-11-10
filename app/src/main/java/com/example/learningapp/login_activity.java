dde5c047aa821ef836e35fc046c289817d042ad8
Szpetko <b.szpetko@gmail.com>
2021-11-10 02:10:10 +0200

    Login Complete

--- app/src/main/java/com/example/learningapp/login_activity.java
+++ app/src/main/java/com/example/learningapp/login_activity.java
@@ -1,6 +1,7 @@
 package com.example.learningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Build;
@@ -11,10 +12,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.learningapp.data.UserDAO;
+import com.example.learningapp.data.UserDatabase;
+import com.example.learningapp.model.User;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class login_activity extends AppCompatActivity {
 
+    //Database
+    private UserDAO userDAO;
 
     //References to Buttons
     private FloatingActionButton fabtn_back;
@@ -36,12 +42,30 @@ public class login_activity extends AppCompatActivity {
         et_password = findViewById(R.id.et_password);
         btn_login = findViewById(R.id.btn_login);
 
+
+        //Database
+        userDAO = Room.databaseBuilder(this, UserDatabase.class, "User").allowMainThreadQueries().build().getUserDao();
+
+
         //Button Listeners
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(login_activity.this, "Info: " + et_username.getText().toString()
-                        + ", " + et_password.getText().toString(), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(login_activity.this, "Info: " + et_username.getText().toString()
+//                        + ", " + et_password.getText().toString(), Toast.LENGTH_SHORT).show();
+
+                String username = et_username.getText().toString().trim();
+                String password = et_password.getText().toString().trim();
+
+                User user = userDAO.getUser(username,password);
+                if (user != null){
+                    Toast.makeText(login_activity.this, "Login Succeful!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(login_activity.this, "UnRegistered user or incorrect password.", Toast.LENGTH_SHORT).show();
+                }
+
+
             }
         });
 
